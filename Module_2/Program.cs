c824e201737f3669e4e37c16e1578052be5f36af
Roman <Aulrata@gmail.com>
2024-08-28 21:23:54 +0300

    User input added

--- Module_2/Program.cs
+++ Module_2/Program.cs
@@ -2,16 +2,30 @@
 
 #region Task_1
 
-double length = 1.5;
+Console.WriteLine("Введите длину окружности");
+string? answer = Console.ReadLine();
+
+double.TryParse(answer, out double length); 
 double radius = length / 2 * Math.PI;
 double square = Math.PI * Math.Pow(radius, 2);
 
 Console.WriteLine($"Радиус круга: {radius}, площадь круга: {square}");
+
 #endregion
 
 #region Task_2
 
-double xA = 1, yA = 1, xB = 3, yB = 3;
+Console.WriteLine("Введите координату x тчоки A прямоугольника");
+double.TryParse(Console.ReadLine(), out double xA);
+
+Console.WriteLine("Введите координату y тчоки A прямоугольника");
+double.TryParse(Console.ReadLine(), out double yA);
+
+Console.WriteLine("Введите координату x тчоки B прямоугольника");
+double.TryParse(Console.ReadLine(), out double xB);
+
+Console.WriteLine("Введите координату y тчоки B прямоугольника");
+double.TryParse(Console.ReadLine(), out double yB);
 
 double width = Math.Abs(xB - xA), height = Math.Abs(yB - yA);
 
@@ -24,11 +38,24 @@ Console.WriteLine($"Периметр прямоугольника: {perimeter}, 
 
 #region Task_3
 
-xA = 1;
-yA = 1;
-xB = 3;
-yB = 3;
-double xC = 3, yC = 1;
+Console.WriteLine("Введите координату x тчоки A треугольника");
+double.TryParse(Console.ReadLine(), out xA);
+
+Console.WriteLine("Введите координату y тчоки A треугольника");
+double.TryParse(Console.ReadLine(), out yA);
+
+Console.WriteLine("Введите координату x тчоки B треугольника");
+double.TryParse(Console.ReadLine(), out xB);
+
+Console.WriteLine("Введите координату y тчоки B треугольника");
+double.TryParse(Console.ReadLine(), out yB);
+
+Console.WriteLine("Введите координату x тчоки C треугольника");
+double.TryParse(Console.ReadLine(), out double xC);
+
+Console.WriteLine("Введите координату y тчоки C треугольника");
+double.TryParse(Console.ReadLine(), out double yC);
+
 
 double ab = Math.Sqrt(Math.Pow(xB-xA, 2) + Math.Pow(yB-yA, 2));
 double ac = Math.Sqrt(Math.Pow(xC-xA, 2) + Math.Pow(yC-yA, 2));
@@ -41,6 +68,5 @@ square = Math.Sqrt(semiPerimeter * (semiPerimeter - ab) * (semiPerimeter - ac) *
 
 Console.WriteLine($"Длина сторон. AB: {ab}, AC: {ac}, BC: {bc}");
 Console.WriteLine($"Периметр треугольника: {perimeter}, площадь треугольника: {square}");
-
 
 #endregion

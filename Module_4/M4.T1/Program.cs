a825563b00ae54d12f9610e769c1f7f48cbda10b
Roman <Aulrata@gmail.com>
2024-09-01 01:53:26 +0300

    Task 1 and 2 done in module 4

---
+++ Module_4/M4.T1/Program.cs
@@ -0,0 +1,11 @@
+﻿
+Console.WriteLine("Введите натуральное число N: ");
+int n = int.Parse(Console.ReadLine());
+int square = 0;
+
+for (int i = 1; i < n + 1; i++)
+{
+    square += 2 * i - 1;
+}
+
+Console.WriteLine($"Ваше число {n} в квадрате равно {square}");
---
+++ Module_4/M4.T2/Program.cs
@@ -0,0 +1,28 @@
+﻿
+Console.WriteLine("Вводите последовательность чисел окончание последовательности = 0");
+int number = 0, positiveNumbers = 0, negativeNumbers = 0;
+do
+{
+    number = int.Parse(Console.ReadLine());
+
+    switch (number)
+    {
+        case > 0:
+            positiveNumbers++;
+            break;
+        case < 0:
+            negativeNumbers++;
+            break;
+    }
+    
+} while (number != 0);
+
+Console.WriteLine($"Кол-во положительный: {positiveNumbers}, кол-во отрицательных: {negativeNumbers}.");
+
+if (positiveNumbers > negativeNumbers)
+    Console.WriteLine("Положительных чисел больше.");
+else if (positiveNumbers == negativeNumbers)
+    Console.WriteLine("Положительных и отрицательных чисел равное количество.");
+else
+    Console.WriteLine("Отрицательных чисел больше.");
+    

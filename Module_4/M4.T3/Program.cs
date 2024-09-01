083d2fd4ecc487a76b5518b36646f192c3353f6f
Roman <Aulrata@gmail.com>
2024-09-01 14:26:49 +0300

    Task 3 in module 4 done. Module 4 done

---
+++ Module_4/M4.T3/Program.cs
@@ -0,0 +1,35 @@
+﻿Console.WriteLine("Введите число A: ");
+int a = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите число B: ");
+int b = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите число C: ");
+int c = int.Parse(Console.ReadLine());
+
+int area = 0;
+int count = 0;
+int areaOfSquare = 0;
+
+while (count < a)
+{
+    area += b;
+    count++;
+}
+
+count = 0;
+
+while (count < c)
+{
+    areaOfSquare += c;
+    count++;
+}
+
+count = 0;
+
+while (areaOfSquare <= area)
+{
+    area -= areaOfSquare;
+    count++;
+}
+Console.WriteLine($"В прямоугольник {a}x{b} поместилось {count} квадратов со стороной {c}");

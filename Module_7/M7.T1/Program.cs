3e3f6ec8dd69a2aea0467d853e1478ee294c72ec
Roman <Aulrata@gmail.com>
2024-09-04 12:32:23 +0300

    Module 7 done

---
+++ Module_7/M7.T1/Program.cs
@@ -0,0 +1,23 @@
+﻿// Два треугольника заданы длинами своих сторон. Определить, площадь какого из них больше (создать метод для вычисления
+// площади  треугольника по длинам его сторон).  Для решения задачи можно использовать формулу Герона
+
+Console.WriteLine("Введите стороны первого треугольника: ");
+double a = double.Parse(Console.ReadLine());
+double b = double.Parse(Console.ReadLine());
+double c = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите стороны второго треугольника: ");
+double a2 = double.Parse(Console.ReadLine());
+double b2 = double.Parse(Console.ReadLine());
+double c2 = double.Parse(Console.ReadLine());
+
+double firstSquare = GetSquare(a, b, c);
+double secondSquare = GetSquare(a, b, c);
+
+Console.WriteLine($"Площадь {(firstSquare > secondSquare ? "первого" : "второго")} треугольника больше");
+
+double GetSquare(double a, double b, double c)
+{
+    double semiPerimeter = (a + b + c) / 2;
+    return Math.Sqrt(semiPerimeter * (semiPerimeter - a) * (semiPerimeter - b) * (semiPerimeter - c));
+}
---
+++ Module_7/M7.T2/Program.cs
@@ -0,0 +1,18 @@
+﻿// Создать метод для вычисления объема и площади поверхности куба по длине его ребра.
+Console.WriteLine("Введите длину ребра куба: ");
+double length = double.Parse(Console.ReadLine());
+
+Console.WriteLine($"Объем куба: {GetVolume(length)}, площадь поверхности куба: {GetSquare(length)}");
+
+double GetVolume(double length)     // Проще было бы с лямдой выражения) double GetVolume(double length) => Math.Pow(length, 3);
+{
+    return Math.Pow(length, 3);
+}
+
+double GetSquare(double length)     // double GetSquare(double length) => Math.Pow(length, 2);
+{
+    return Math.Pow(length, 2);
+}
+
+
+

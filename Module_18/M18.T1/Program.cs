ef294b1deecb0e677bafcb2cec5b74032367512e
Roman <Aulrata@gmail.com>
2024-10-06 23:24:19 +0300

    Module 18 done

---
+++ Module_18/M18.T1/Program.cs
@@ -0,0 +1,25 @@
+﻿
+string str = "([]{})[]";
+bool flag = true;
+
+Stack<char> stack = new Stack<char>();
+
+foreach (var item in str)
+{
+    if (item is '(' or '{' or '[')
+    {
+        stack.Push(item);
+        continue;
+    }
+
+    if ((item == '[' && stack.Pop() != ']') || (item == '(' && stack.Pop() != ')') || (item == '{' && stack.Pop() != '}'))
+    {
+        flag = false;
+        break;
+    }
+}
+
+if (stack.Count != 0)
+    flag = false;
+
+Console.WriteLine(flag ? "Правильная скобочная последовательность" : "Не правильная скобочная последовательность");

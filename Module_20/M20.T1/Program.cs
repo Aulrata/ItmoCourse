598fad0faca364e08695f066e223815609b950ce
Roman <Aulrata@gmail.com>
2024-10-18 21:37:51 +0300

    Module 20 done

---
+++ Module_20/M20.T1/Program.cs
@@ -0,0 +1,30 @@
+﻿var myDelegate = new MyDelegate(CircleLength);
+myDelegate += CircleSquare;
+myDelegate += CircleVolume;
+
+Console.Write("Введите радиус: ");
+var radius = int.Parse(Console.ReadLine());
+myDelegate(radius);
+
+static double CircleLength(double r)
+{
+    var length = 2 * Math.PI * r;
+    Console.WriteLine($"Длина окружности: {length}");
+    return length;
+}
+
+static double CircleSquare(double r)
+{
+    var square = Math.PI * Math.Pow(r, 2);
+    Console.WriteLine($"Площадь окружности: {square}");
+    return square;
+};
+
+static double CircleVolume(double r)
+{
+    var volume = 4.0 / 3  *Math.PI * Math.Pow(r, 3);
+    Console.WriteLine($"Объем окружности: {volume}");
+    return volume;
+};
+
+delegate double MyDelegate(double number);

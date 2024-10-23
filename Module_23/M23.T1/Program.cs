e686da2f35ed35bd2a8f3734325001854b248f78
Roman <Aulrata@gmail.com>
2024-10-23 13:34:43 +0300

    Module 23 done

---
+++ Module_23/M23.T1/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите число для факториала");
+int number = int.Parse(Console.ReadLine());
+var factorial = CountFactorial(number);
+Console.WriteLine($"Main работает");
+
+
+Console.WriteLine($"{number}! = {factorial.Result}");
+
+async Task<long> CountFactorial(int number)
+{
+    if (number == 1)
+        return number;
+
+    return CountFactorial(number - 1).Result * number;
+}
